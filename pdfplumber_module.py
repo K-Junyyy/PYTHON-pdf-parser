4d7bb600832906ce7920b97d7840da68faf1f03e
Hongdeok Jun <wjsghdejr12@korea.ac.kr>
2021-12-21 01:24:47 +0900

    pdf parser

--- pdfplumber_module.py
+++ pdfplumber_module.py
@@ -1,20 +1,49 @@
 import pdfplumber
+import re
 
-text =""
-with pdfplumber.open("./21_391_13.pdf") as pdf:
-    print("총 페이지는 ",len(pdf.pages))
+txt = ""
+
+with pdfplumber.open("./21_391_11.pdf") as pdf:
     for i in range(len(pdf.pages)):
-        now_page = pdf.pages[i]
-        text += now_page.extract_text()
+        page = pdf.pages[i];
+        l = page.crop((0, 0, 0.5 * float(page.width), float(page.height))).extract_text();
+        r = page.crop((0.5 * float(page.width), 0.0, float(page.width), float(page.height))).extract_text();
+        txt += str(l)
+        txt += str(r)
 
-attendanceStart = text.find("출석 의원")
-attendanceEnd = text.find("국회 참석자")
-attendance = text[attendanceStart:attendanceEnd]
+present_congressman = []
+flag = False
+split_txt = txt.split('\n')
+for sentence in split_txt:
+    if flag == True:
+        if sentence.find('◯') != -1:
+            break
+        splited_sentence = sentence.split('     ')
+        short_name = ''
+        for idx in range(len(splited_sentence)):
+            if len(splited_sentence[idx]) > 10 and len(splited_sentence[idx].split('   ')) > 1:
+                splited_sentence.append(splited_sentence[idx].split('   ')[1])
+                splited_sentence[idx] = splited_sentence[idx].split('   ')[0]
+        for name in splited_sentence:
+            name = name.strip()
+            if len(name) > 10:
+                continue
+            elif len(name) > 2:
+                present_congressman.append(name)
+            elif short_name == '':
+                short_name += name
+            else:
+                short_name += name
+                present_congressman.append(short_name)
+                short_name = ''
+    if sentence.find('출석 의원') != -1:
+        flag = True
 
-def extractAttendance(str):
-    start = attendance.find(str)
-    result = attendance[start:]
-    end = result.find("◯")
-    print(result[:end])
+names = ""
+for name in present_congressman:
+    names += name.replace(" ", '') + '\n'
 
-extractAttendance("출석 의원")
+print(str(len(present_congressman)) + "명")
+
+with open("./present_congressman.txt", "w", -1, 'utf-8') as file:
+    file.write(names)
